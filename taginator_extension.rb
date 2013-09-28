65babdbc8186705b13921ac8a245b4a3ea72d580
Michal Cichra <michal@o2h.cz>
2013-09-28 21:43:59 +0200

    category list

--- taginator_extension.rb
+++ taginator_extension.rb
@@ -4,16 +4,16 @@ class TaginatorExtension < Radiant::Extension
   version Taginator::VERSION
   description "This extension enhances the page model with tagging capabilities, tagging as in \"2.0\" and tagclouds."
   url "http://github.com/mikz/radiant-taginator-extension"
-  
+
   DEFAULT_RESULTS_URL = '/t'
- 
+
   def activate
     config = Radiant::Config
     if config.table_exists?
       config['tags.results_page_url'] = config['tags.results_page_url'].presence || DEFAULT_RESULTS_URL
       config['tags.complex_strings'] = config['tags.complex_strings'].presence || false
     end
-    
+
     Page.class_eval do
       acts_as_taggable_on :categories
 
@@ -23,21 +23,21 @@ class TaginatorExtension < Radiant::Extension
 
       alias :tag_list :category_list
       alias :tag_list= :category_list=
-      
+
     end
-    
+
     TagSearchPage
     Page.send :include, Taginator::Tags
     admin.page.edit.add :extended_metadata, 'tag_field'
 
-    
+
 
     # HELP
     if admin.respond_to?(:help)
       admin.help.index.add :page_details, 'using_tags', :after => 'breadcrumbs'
     end
   end
-  
+
   def deactivate
   end
 end
